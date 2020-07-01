9d65b8783996547831149955602a4e91078f2d0e
Gabor Cserto <csertog@gmail.com>
2020-07-01 16:38:32 +0200

    Adding placeholder content

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Col, Row} from 'react-bootstrap';
+import {Container, Col, ListGroup, Row, Tab} from 'react-bootstrap';
 import './App.scss';
 
 import Header from '../../components/Header/Header.jsx';
@@ -67,7 +67,36 @@ class App extends React.Component {
 				<Header />
 				<div className="main__content">
 					<h2>Korábbi nyerőszámok</h2>
-					<div className="previousLottoNumbers" />
+					<Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1" >
+						<Row className="previousLottoNumbers">
+							<Col sm={4}>
+								<ListGroup>
+									<ListGroup.Item action href="#link1">
+										Ötös lottó
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link2">
+										Hatos lottó
+									</ListGroup.Item>
+									<ListGroup.Item action href="#link3">
+										Skandináv lottó
+									</ListGroup.Item>
+								</ListGroup>
+							</Col>
+							<Col sm={8}>
+								<Tab.Content>
+									<Tab.Pane eventKey="#link1">
+										A június 27-én kisorsolt nyerőszámok:
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link2">
+										A június 28-án kisorsolt nyerőszámok:
+									</Tab.Pane>
+									<Tab.Pane eventKey="#link3">
+										A június 24-én kisorsolt nyerőszámok:
+									</Tab.Pane>
+								</Tab.Content>
+							</Col>
+						</Row>
+					</Tab.Container>
 					<h2>Lottó számok generálása</h2>
 					{randomLottoNumbers}
 				</div>
