6587c6dc86db45b010ff21a91e316d1d99c5db8b
Gabor Cserto <csertog@gmail.com>
2020-07-31 22:16:23 +0200

    Fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,48 +9,51 @@ const Header = props => {
 	const handleClick = () => setAnimate(!animate);
 
 	return (
-		<Navbar bg="dark" variant="dark" fixed="top" expand="md" className="header">
-			<NavLink className="navbar-brand"
-			         variant="danger"
-			         exact to="/">
-				<FaChartBar /> Lottó statisztika
-			</NavLink>
-			<button
-				onClick={handleClick}
-				type="button"
-				aria-label="Toggle navigation"
-				className={animate ? 'navbar-toggler collapsed open' : 'navbar-toggler collapsed'}>
-				<div />
-			</button>
-			<Navbar.Collapse id="basic-navbar-nav" className={animate ? 'open' : null}>
-				<Nav className="mr-auto">
-					<NavLink
-						activeClassName="active"
-						className="nav-link"
-						exact to="/">
-						Kezdőlap
-					</NavLink>
-					<NavLink
-						activeClassName="active"
-						className="nav-link"
-						to="/otos">
-						Ötös lottó
-					</NavLink>
-					<NavLink
-						activeClassName="active"
-						className="nav-link"
-						to="/hatos">
-						Hatos lottó
-					</NavLink>
-					<NavLink
-						activeClassName="active"
-						className="nav-link"
-						to="/hetes">
-						Skandináv lottó
-					</NavLink>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
+		<React.Fragment>
+			<Navbar bg="dark" variant="dark" fixed="top" expand="md" className="header">
+				<NavLink className="navbar-brand"
+				         variant="danger"
+				         exact to="/">
+					<FaChartBar /> Lottó statisztika
+				</NavLink>
+				<button
+					onClick={handleClick}
+					type="button"
+					aria-label="Toggle navigation"
+					className={animate ? 'navbar-toggler collapsed open' : 'navbar-toggler collapsed'}>
+					<div />
+				</button>
+				<Navbar.Collapse id="basic-navbar-nav" className={animate ? 'open' : null}>
+					<Nav className="mr-auto">
+						<NavLink
+							activeClassName="active"
+							className="nav-link"
+							exact to="/">
+							Kezdőlap
+						</NavLink>
+						<NavLink
+							activeClassName="active"
+							className="nav-link"
+							to="/otos">
+							Ötös lottó
+						</NavLink>
+						<NavLink
+							activeClassName="active"
+							className="nav-link"
+							to="/hatos">
+							Hatos lottó
+						</NavLink>
+						<NavLink
+							activeClassName="active"
+							className="nav-link"
+							to="/hetes">
+							Skandináv lottó
+						</NavLink>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			{animate ? <div className="overlay"/> : null}
+		</React.Fragment>
 	);
 }
 
