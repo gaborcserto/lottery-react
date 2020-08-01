5110e066a09840a1e1656de463e45a05eac29051
Gabor Cserto <csertog@gmail.com>
2020-08-01 02:30:16 +0200

    Updates

---
+++ precache-manifest.9ccd69c002aff11440d5afa0dea2c472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4ab3e72132a07dd4e7b9849b54e36a",
+    "url": "/lottery-react/index.html"
+  },
+  {
+    "revision": "a2245678ddf4539f290c",
+    "url": "/lottery-react/static/css/main.5dfdecea.chunk.css"
+  },
+  {
+    "revision": "c4a9543095235bc4aa74",
+    "url": "/lottery-react/static/js/2.05c608a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lottery-react/static/js/2.05c608a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2245678ddf4539f290c",
+    "url": "/lottery-react/static/js/main.15f2a6fd.chunk.js"
+  },
+  {
+    "revision": "7269d26405c47f3d7638",
+    "url": "/lottery-react/static/js/runtime-main.a8cb40af.js"
+  },
+  {
+    "revision": "3a4314f2856d4185ac3237d9f58bdf79",
+    "url": "/lottery-react/static/media/sprite-game-logos.3a4314f2.png"
+  }
+]);
