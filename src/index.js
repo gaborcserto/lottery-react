187a21d19d130d19d66436b41ebe6dab3cc30292
Gabor Cserto <csertog@gmail.com>
2023-04-28 21:13:15 +0200

    Fixed some issues

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
 root.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/lottery-react">
         <App/>
       </BrowserRouter>
     </React.StrictMode>,
