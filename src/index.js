d8f0d25df8fa684572939980f6d85896934bfd3d
Gabor Cserto <csertog@gmail.com>
2023-04-29 02:29:07 +0200

    Fixed some css issues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import  { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
 import './index.scss';
 import App from './containers/App';
@@ -11,14 +11,14 @@ if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = createRoot(rootElement);
 root.render(
-    <React.StrictMode>
-      <BrowserRouter basename="/lottery-react">
-        <App/>
-      </BrowserRouter>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename="/lottery-react">
+      <App/>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
